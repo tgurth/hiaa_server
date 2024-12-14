41c7e40c38142b69a5086ea3f75ac191db66ed96
Tyler Gurth <90432221+tgurth@users.noreply.github.com>
2024-12-14 15:55:10 -0500

    Update embedding_server.py

--- embedding_server.py
+++ embedding_server.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 import tensorflow_hub as hub
-from flask_cors import CORS, cross_origin
-import numpy as np
+from flask_cors import CORS
+import os
 
 # Universal Sentence Encoder model
 use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
 # Flask App
 app = Flask(__name__)
 CORS(app, origins="https://tylergurth.com", methods=["GET", "POST"])
@@ -23,4 +24,6 @@ def get_embedding():
     return jsonify({'embedding': embedding})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
